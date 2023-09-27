adad0d40980213ed5ce164db6570e7e057bd0d7a
R-G2110 <argeldelacruz86@gmail.com>
2023-09-27 20:23:16 +0200

    [Add]: change photo method on click

---
+++ js/dbPhoto.js
@@ -0,0 +1,13 @@
+const photo = [
+	{ image: 'img/01.webp'
+	}, {
+		image: 'img/02.webp'
+	}, {
+		image: 'img/03.webp'
+	}, {
+		image: 'img/04.webp'
+	}, {
+		image: 'img/05.webp'
+	}];
+	
+export { photo };
--- js/script.js
+++ js/script.js
@@ -1,9 +1,28 @@
+// import { photo } from "./dbPhoto.js";
 const {createApp} = Vue;
+let index = 0; 
 
 createApp({
 	data() {
 		return {
-			message: 'Hello Vue!'
+			message: 'Hello Vue!',
+			index: 0,
+			linkHref: 'https://unsplash.it/600/300?image=171',
+			imageSource: ['img/01.webp',
+									  'img/02.webp',
+									  'img/03.webp',
+									  'img/04.webp',
+									  'img/05.webp'
+			]
 		}
 	},
+
+	methods: {
+
+		indexPlusOne(){
+			this.index++;
+			if (this.index === this.imageSource.length) this.index =  0
+			console.log(this.imageSource, this.index);
+		}
+	}
 }).mount('#app')
